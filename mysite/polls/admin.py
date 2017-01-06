b23fe53ece360e8651d8f90b788576981057665f
WSL <167372@zte.com.cn>
2017-01-06 17:10:00 +0800

    part7 half

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question
+from .models import Choice
 
-admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['question_txt', 'put_date']
+    fieldsets = [
+        (None,                  {'fields': ['question_txt']}),
+        ('Date information',    {'fields': ['put_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_txt', 'put_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
